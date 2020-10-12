ecdf704bbb88181677432f8234c35fd976136176
JuanGF <jugaybre@gmail.com>
2020-10-12 08:42:02 -0500

    Updates

---
+++ precache-manifest.1fc97b1d85abbc1ece70ea37378a4c05.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1832c3b43fd7189b5539948f17f61a",
+    "url": "/CityRestaurants/index.html"
+  },
+  {
+    "revision": "cdbf3e288884969317a8",
+    "url": "/CityRestaurants/static/css/main.5eb8ef16.chunk.css"
+  },
+  {
+    "revision": "88ffbb507e745c934597",
+    "url": "/CityRestaurants/static/js/2.ac436d8d.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/CityRestaurants/static/js/2.ac436d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdbf3e288884969317a8",
+    "url": "/CityRestaurants/static/js/main.30f3c9fb.chunk.js"
+  },
+  {
+    "revision": "e69e5bdc699b2d195465",
+    "url": "/CityRestaurants/static/js/runtime-main.28015606.js"
+  },
+  {
+    "revision": "a002cf261d38dde4826924be40127aba",
+    "url": "/CityRestaurants/static/media/demo.a002cf26.jpg"
+  },
+  {
+    "revision": "1b7a8188fb79ec69530048e02f83e2ab",
+    "url": "/CityRestaurants/static/media/icomoon.1b7a8188.svg"
+  },
+  {
+    "revision": "2d128f461d6782144b2c5e297d7e7490",
+    "url": "/CityRestaurants/static/media/icomoon.2d128f46.woff"
+  },
+  {
+    "revision": "6fe591f9007eab0fec1d8ca055b59e57",
+    "url": "/CityRestaurants/static/media/icomoon.6fe591f9.ttf"
+  },
+  {
+    "revision": "b9fef8749781a48b0fecc6baf69f3cab",
+    "url": "/CityRestaurants/static/media/icomoon.b9fef874.eot"
+  }
+]);
